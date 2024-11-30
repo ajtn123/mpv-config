a7232f446d8a8c7fafb957eff92322fa72b2497b
ajtn123 <Ajitani-Hifumi@outlook.com>
2024-11-30 18:59:06 +0800

    fix another day past

--- scripts/opt.lua
+++ scripts/opt.lua
@@ -2,7 +2,7 @@ local mp = require 'mp'
 
 function TableLeng(t)
     local leng = 0
-    for k, v in pairs(t) do
+    for _, _ in pairs(t) do
         leng = leng + 1
     end
     return leng;
@@ -23,16 +23,15 @@ function Info(name, value)
     mp.msg.info(name .. ': ' .. value)
 end
 
-function ShowTable(arg, table, layer)
+function ShowTable(arg, propTable, layer)
     mp.msg.info(arg)
     local pos = 0
-    for index, value in pairs(table) do
+    for index, value in pairs(propTable) do
         pos = pos + 1
-
         if type(value) == "table" then
             ShowTable(index, value, layer + 1)
         end
-        if pos == TableLeng(table) then
+        if pos == TableLeng(propTable) then
             Info((' │'):rep(layer) .. ' └' .. index, value)
         else
             Info((' │'):rep(layer) .. ' ├' .. index, value)
@@ -52,7 +51,7 @@ function ShowOpts(...)
     local args = { ... }
     for _, arg in ipairs(args) do
         local property_value = mp.get_property_native(arg)
-        if property_value then ShowMsg(arg, property_value) end
+        ShowMsg(arg, property_value)
     end
 end
 
