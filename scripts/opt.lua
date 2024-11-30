e982dbbdd8ac43905b3cfe9848186f26de207feb
ajtn123 <Ajitani-Hifumi@outlook.com>
2024-11-30 18:09:46 +0800

    improvements

--- scripts/opt.lua
+++ scripts/opt.lua
@@ -23,21 +23,28 @@ function Info(name, value)
     mp.msg.info(name .. ': ' .. value)
 end
 
+function ShowTable(arg, table, layer)
+    mp.msg.info(arg)
+    local pos = 0
+    for index, value in pairs(table) do
+        pos = pos + 1
+
+        if type(value) == "table" then
+            ShowTable(index, value, layer + 1)
+        end
+        if pos == TableLeng(table) then
+            Info((' │'):rep(layer) .. ' └' .. index, value)
+        else
+            Info((' │'):rep(layer) .. ' ├' .. index, value)
+        end
+    end
+end
+
 function ShowMsg(arg, property_value)
     if type(property_value) == "table" then
-        mp.msg.info(arg)
-        local pos = 0
-        for index, value in pairs(property_value) do
-            pos = pos + 1
-            if pos == TableLeng(property_value) then
-                Info(' └' .. index, value)
-            else
-                Info(' ├' .. index, value)
-            end
-        end
+        ShowTable(arg, property_value, 0)
         return
     end
-
     Info(arg, property_value)
 end
 
